424de19fd8b3f2e68610020c2167b2c4577e9cbd
Elen Davtyan <149108366+ElenDavtyan08@users.noreply.github.com>
2024-04-16 21:40:04 +0400

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -34,7 +34,7 @@ def generate_password(min_lenght, numbers = True, special_characters = True):
 
     return pwd
 
-min_length = int(input('Enter the minimum length: '))
+min_length = int(input('Enter the minimum length: '))  # min length
 has_number = input('Do you want to have numbers (y/n)? ').lower() == 'y'
 has_special = input('Do you want to have special characters (y/n)? ').lower() == 'y'
 pwd = generate_password(min_length, has_number, has_special)
